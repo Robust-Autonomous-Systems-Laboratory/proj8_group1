37226003a6cb29345ae28e4a57f5cbe7156cb89f
Reid <rabeckes@mtu.edu>
2026-04-04 10:38:46 -0400

    Finished patrol node (I think)

--- patrol/patrol/patrol_node.py
+++ patrol/patrol/patrol_node.py
@@ -1,5 +1,4 @@
 import rclpy
-from rclpy.node import Node
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from geometry_msgs.msg import PoseStamped
 
@@ -12,16 +11,66 @@ def main()->None:
 
     # Define patrol points
     patrol_points = [
-        PoseStamped(header=None, pose=navigator.getCurrentPose().pose),  # Start point
-        PoseStamped(header=None, pose=navigator.getCurrentPose().pose),  # Point 1
-        PoseStamped(header=None, pose=navigator.getCurrentPose().pose),  # Point 2
-        PoseStamped(header=None, pose=navigator.getCurrentPose().pose)   # Point 3
+        PoseStamped(),  # Point 0
+        PoseStamped(),  # Point 1
+        PoseStamped(),  # Point 2
+        PoseStamped(),  # Point 3
+        PoseStamped()   # Point 4 - Start Point
     ]
 
     # Load patrol points (replace with actual coordinates)
-    # TODO - load yaml of patrol points
+    # Waypoint 0
+    patrol_points[0].header.frame_id = 'map'
+    patrol_points[0].header.stamp = navigator.get_clock().now().to_msg()
+    patrol_points[0].pose.position.x = 5.481
+    patrol_points[0].pose.position.y = 0.697
+    patrol_points[0].pose.orientation.z = 0.646
+    patrol_points[0].pose.orientation.w = 0.763
+    # Waypoint 1
+    patrol_points[1].header.frame_id = 'map'
+    patrol_points[1].header.stamp = navigator.get_clock().now().to_msg()
+    patrol_points[1].pose.position.x = 5.099
+    patrol_points[1].pose.position.y = 4.019
+    patrol_points[1].pose.orientation.z = 0.999
+    patrol_points[1].pose.orientation.w = 0.038
+    # Waypoint 2
+    patrol_points[2].header.frame_id = 'map'
+    patrol_points[2].header.stamp = navigator.get_clock().now().to_msg()
+    patrol_points[2].pose.position.x = -1.785
+    patrol_points[2].pose.position.y = 2.106
+    patrol_points[2].pose.orientation.z = -0.748
+    patrol_points[2].pose.orientation.w = 0.664
+    # Waypoint 3
+    patrol_points[3].header.frame_id = 'map'
+    patrol_points[3].header.stamp = navigator.get_clock().now().to_msg()
+    patrol_points[3].pose.position.x = -0.984
+    patrol_points[3].pose.position.y = -3.599
+    patrol_points[3].pose.orientation.z = -0.833
+    patrol_points[3].pose.orientation.w = 0.554
+    # Waypoint 4
+    patrol_points[4].header.frame_id = 'map'
+    patrol_points[4].header.stamp = navigator.get_clock().now().to_msg()
+    patrol_points[4].pose.position.x = 2.764
+    patrol_points[4].pose.position.y = -4.074
+    patrol_points[4].pose.orientation.z = 0.826
+    patrol_points[4].pose.orientation.w = 0.562
 
     # Start patrol loop
     task = navigator.followWaypoints(patrol_points)
-    # TODO - check task progress
+    while not navigator.isTaskComplete(task=follow_waypoints_task):
+        feedback = navigator.getFeedback(task=follow_waypoints_task)
+        navigator.get_logger().info(f"Navigating to waypoint {feedback.current_waypoint}")
+
+    result = navigator.getResult();
+    if result == TaskResult.SUCCEEDED:
+        navigator.get_logger().info('Goal succeeded!')
+    elif result == TaskResult.CANCELED:
+        navigator.get_logger().info('Goal was canceled!')
+    elif result == TaskResult.FAILED:
+        (error_code, error_msg) = navigator.getTaskError()
+        navigator.get_logger().error('Goal failed!{error_code}:{error_msg}')
+    else:
+        print('Goal has an invalid return status!')
+
+    navigator.lifecycleShutdown()
 
