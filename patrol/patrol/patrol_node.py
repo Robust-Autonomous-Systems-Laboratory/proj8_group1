76222e30fbcdebadcb67f969c48a26452e38cc40
Reid <rabeckes@mtu.edu>
2026-04-06 20:12:01 -0400

    Added drift check

--- patrol/patrol/patrol_node.py
+++ patrol/patrol/patrol_node.py
@@ -1,7 +1,19 @@
 import rclpy
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 import argparse
+
+lastPose = None
+
+def amcl_cb(msg):
+    lastPose = msg
+
+def drift(amcl : PoseWithCovarianceStamped, waypoint : PoseStamped):
+    if lastPose is None:
+        return -1.0, -1.0
+    x_drift = abs(amcl.pose.pose.position.x - waypoint.pose.position.x)
+    y_drift = abs(amcl.pose.pose.position.y - waypoint.pose.position.y)
+    return x_drift, y_drift
 
 def main()->None:
     # Get args
@@ -13,7 +25,14 @@ def main()->None:
     rclpy.init(args=other)
     navigator = BasicNavigator()
 
+    amcl_sub = navigator.create_subscription(
+            PoseWithCovarianceStamped,
+            '/amcl_pose',
+            amcl_cb,
+            10)
+
     # Wait for the navigation stack to be ready
+    navigator.get_logger().info("Waiting for Nav2 stack...")
     navigator.waitUntilNav2Active()
 
     # Define patrol points
@@ -73,6 +92,9 @@ def main()->None:
         result = navigator.getResult();
         if result == TaskResult.SUCCEEDED:
             navigator.get_logger().info('Goal succeeded!')
+            # Calculate drift
+            x, y = drift(lastPose, patrol_points[4])
+            navigator.get_logger().info(f"Dift accumulated x: {x}, y: {y}")
         elif result == TaskResult.CANCELED:
             navigator.get_logger().info('Goal was canceled!')
         elif result == TaskResult.FAILED:
@@ -80,6 +102,7 @@ def main()->None:
             navigator.get_logger().error('Goal failed!{error_code}:{error_msg}')
         else:
             navigator.get_logger().error('Goal has an invalid return status!')
+        
 
     navigator.lifecycleShutdown()
 
